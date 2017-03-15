8b90e1a60eccf4583fd48c7108801b75705f83ad
GLaDOS-096 <ZhangYf_CPU@163.com>
2017-03-15 20:48:20 +0800

    work out the first Medusa.extend()

--- test/medusa.test.js
+++ test/medusa.test.js
@@ -1,6 +1,36 @@
 // used as a unit test container
 // always the latest patterns inside
 
-var getUserAgent = function(){
-    return navigator.userAgent;
+var Medusa = {
+
+    /*  i'm still thinking about it. 
+        i guess i have to remove this finally out of the 'basic' props.
+     */
+    getUserAgent: function getUserAgent(){
+        return navigator.userAgent;
+    },
+    parseUserAgent: function parseUserAgent(user_agent){
+        var _uas1 = user_agent.split('(');
+        var h1 = _uas1[0];
+        var _uas2 = _uas1[1].split(')');
+        var browser_str = _uas2[1].replace(/(^\s*)|(\s*$)/g, "").split(' ');
+        var os_str = _uas2[0].split(' ');
+        return {
+            header: h1,
+            browser_str: browser_str,
+            os_str: os_str
+        }
+    },
+
+    /* this Medusa.extend() turns out to be all folly 
+     */
+    extend: function extend(props){
+        props.forEach(function(item,index){
+            if (item.name===""){
+                console.error("extension invalid: function name missing.")
+            } else {
+                Medusa[item.name] = item;
+            }
+        })
+    }
 }
