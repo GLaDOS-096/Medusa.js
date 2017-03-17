6459fe4b989c20d4c534bc423cd6a7fee4aabf48
GLaDOS-096 <ZhangYf_CPU@163.com>
2017-03-17 23:49:16 +0800

    rewrite user_agent_check and Medusa.extend() bug fixed on IE

--- modules/user_agent_check.js
+++ modules/user_agent_check.js
@@ -1,25 +1,76 @@
-module.exports = user_agent_check
-
 var user_agent_check = [
     function getUserAgent(){
         return navigator.userAgent;
     },
-    function parseUserAgent(user_agent){
-        var _uas1 = user_agent.split('(');
-        var h1 = _uas1[0];
-        var _uas2 = _uas1[1].split(')');
-        var browser_str = _uas2[1].replace(/(^\s*)|(\s*$)/g, "").split(' ');
-        var os_str = _uas2[0].split(' ');
-        return {
-            head: h1,
-            browser_str: browser_str,
-            os_str: os_str
+    function catchKeyword(ua,keyword){
+        switch(keyword){
+            case "Mozilla":
+            case "Firefox":
+            case "AppleWebKit":
+            case "Safari":
+            case "Version":
+                var re = ua.match(RegExp(keyword+"\/[0-9]*\.[0-9]*"))
+                if (re==null){
+                    return -1
+                } else {
+                    return re[0].replace("/",": ")
+                }
+                break
+            case "Chrome":
+            case "OPR":
+                var re = ua.match(RegExp(keyword+"\/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*"))
+                if (re==null){
+                    return -1
+                } else {
+                    return re[0].replace("/",": ")
+                }
+                break
+            case "Gecko":
+                var re = ua.match(RegExp(keyword+"\/[0-9]*"))
+                if (re==null){
+                    return -1
+                } else {
+                    return re[0].replace("/",": ")
+                }
+                break
+            default:
+                console.error("keyword invalid: not in the dict.")
+                return -1
         }
     },
-    function parseSection(section){
-        return {
-            "section_name": section.split('/')[0],
-            "section_val": section.split('/')[1]
-        }
+    function getUABrowser(){
+        var ua = Medusa.getUserAgent();
+        var dict = ["Firefox","Chrome","Safari","OPR","AppleWebKit","Gecko","Version"]
+        var result_map = [0,0,0,0,0,0,0]
+        var result = ["","","","","","",""]
+        dict.forEach(function(item,index){
+            var re = Medusa.catchKeyword(ua,item)
+            if (re==-1){
+                result_map[index] = 0
+            } else {
+                result_map[index] = 1
+                result[index] = re
+            }
+        })
+        var __re__ = (function(result,result_map){
+            console.log(result_map)
+            switch(result_map){
+                case [1,0,0,0,0,1,0]:
+                   return result[0]
+                   break
+                case [0,1,1,0,1,0,0]:
+                    return result[1]
+                    break
+                case [0,0,1,0,1,0,1]:
+                    return result[2]
+                    break
+                case [0,1,1,1,1,0,0]:
+                    return result[3]
+                    break
+                default:
+                    return "Unknown Browser"
+            }
+        })(result,result_map)
+        return __re__
     }
 ]
--- target/medusa.core.js
+++ target/medusa.core.js
@@ -6,6 +6,8 @@ var Medusa = {
         props.forEach(function(item,index){
             if (item.name===""){
                 console.error("extension invalid: function name missing.")
+            } else if (item.name==undefined){
+                Medusa[item.toLocaleString().split('(')[0].split(' ')[1]] = item;
             } else {
                 Medusa[item.name] = item;
             }
